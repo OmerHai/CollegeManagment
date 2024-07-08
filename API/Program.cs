c9ce585d237ef48193b8bdd06954051e2a83d085
Omer Haimovich <omer16493@gmail.com>
2024-07-08 16:15:24 +0300

    Create user repository

--- API/Program.cs
+++ API/Program.cs
@@ -1,13 +1,16 @@
 using API.Data;
+using API.Data.Repositories;
+using API.Interfaces.Users;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 app.MapControllers();
